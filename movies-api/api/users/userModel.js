8e40c582b7869414e0b8686f2c16e5b90b143e7f
Piotr Płaczek <piotrpdev@gmail.com>
2024-12-08 13:43:44 +0000

    Salt and Hash

--- movies-api/api/users/userModel.js
+++ movies-api/api/users/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const Schema = mongoose.Schema;
 
@@ -13,5 +14,30 @@ const passwordValidator = (password) => {
   return passwordRegex.test(password);
 }
 UserSchema.path("password").validate(passwordValidator);
+
+UserSchema.methods.comparePassword = async function (passw) { 
+  return await bcrypt.compare(passw, this.password); 
+}
+
+UserSchema.statics.findByUserName = function (username) {
+  return this.findOne({ username: username });
+};
+
+UserSchema.pre('save', async function(next) {
+  const saltRounds = 10; // You can adjust the number of salt rounds
+  //const user = this;
+  if (this.isModified('password') || this.isNew) {
+    try {
+      const hash = await bcrypt.hash(this.password, saltRounds);
+      this.password = hash;
+      next();
+  } catch (error) {
+     next(error);
+  }
+
+  } else {
+      next();
+  }
+});
 
 export default mongoose.model('User', UserSchema);
