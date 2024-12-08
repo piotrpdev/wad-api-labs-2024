9e30c08a6f7279230b21a346c432ddaf5abd65c0
Piotr Płaczek <piotrpdev@gmail.com>
2024-12-08 13:20:28 +0000

    Mongo lab Users Router

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -2,6 +2,8 @@ import dotenv from 'dotenv';
 import express from 'express';
 import tasksRouter from './api/tasks';
 import './db';
+import usersRouter from './api/users';
+
 
 dotenv.config();
 
@@ -22,6 +24,8 @@ const port = process.env.PORT;
 app.use(express.json());
 
 app.use('/api/tasks', tasksRouter);
+
+app.use('/api/users', usersRouter);
 
 app.use(errHandler);
 
